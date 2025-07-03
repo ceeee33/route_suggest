1f7d773d35cd36fb38e1cf26b80a4d4fb80014a2
ceeee33 <118290498+ceeee33@users.noreply.github.com>
2025-07-04 03:34:56 +0800

    Delete main.py

--- main.py
+++
@@ -1,6 +0,0 @@
-from fastapi import FastAPI
-from route_api_parse import app as route_app  # If you already created the app in route_api_parse
-
-app = FastAPI()
-
-app.mount("/", route_app)  # Mount your actual FastAPI app
